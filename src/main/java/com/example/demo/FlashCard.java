ce673fb938764aee0e1bf993355675f6a7e3d67f
Camilo Lavado <93832685+Kroszover@users.noreply.github.com>
2023-01-19 19:40:31 -0300

    Avances y app/properties

--- src/main/java/com/example/demo/FlashCard.java
+++ src/main/java/com/example/demo/FlashCard.java
@@ -4,9 +4,72 @@
 package com.example.demo;
 
 /**
- * @author camil
+ * @author Camilo Lavado
  *
  */
 public class FlashCard {
 
+	
+	    private String titulo;
+	    private String descripcion;
+	    private String comentario;
+	    private String tema;
+	    private String categoria;
+	    private int id;
+	    
+	    
+	    
+	    /*Getters y Setters*/
+	    
+		public String getTitulo() {
+			return titulo;
+		}
+		public void setTitulo(String titulo) {
+			this.titulo = titulo;
+		}
+		public String getDescripcion() {
+			return descripcion;
+		}
+		public void setDescripcion(String descripcion) {
+			this.descripcion = descripcion;
+		}
+		public String getComentario() {
+			return comentario;
+		}
+		public void setComentario(String comentario) {
+			this.comentario = comentario;
+		}
+		public String getTema() {
+			return tema;
+		}
+		public void setTema(String tema) {
+			this.tema = tema;
+		}
+		public String getCategoria() {
+			return categoria;
+		}
+		public void setCategoria(String categoria) {
+			this.categoria = categoria;
+		}
+		public int getId() {
+			return id;
+		}
+		public void setId(int id) {
+			this.id = id;
+		}
+		
+		/*Constructor*/
+		public FlashCard(String titulo, String descripcion, String comentario, String tema, String categoria, int id) {
+			super();
+			this.titulo = titulo;
+			this.descripcion = descripcion;
+			this.comentario = comentario;
+			this.tema = tema;
+			this.categoria = categoria;
+			this.id = id;
+		}
+	    
+	
+
+	
 }
