def0320f90b197246a90687058eed9e67750d119
Camilo Lavado <93832685+Kroszover@users.noreply.github.com>
2023-01-19 18:42:04 -0300

    Creacion de clases y conexiones

---
+++ src/main/java/com/example/demo/HibernateUtil.java
@@ -0,0 +1,34 @@
+package com.example.demo;
+
+import org.hibernate.HibernateException;
+import org.hibernate.SessionFactory;
+import org.hibernate.cfg.Configuration;
+import org.hibernate.service.ServiceRegistry;
+import org.hibernate.boot.registry.StandardServiceRegistryBuilder;
+
+
+public class HibernateUtil {
+
+    private static SessionFactory sessionFactory;
+
+    public static void buildSessionFactory() {
+        try {
+            Configuration configuration = new Configuration();
+            configuration.configure("hibernate.cfg.xml");
+            ServiceRegistry serviceRegistry = new StandardServiceRegistryBuilder()
+                    .applySettings(configuration.getProperties()).build();
+            sessionFactory = configuration.buildSessionFactory(serviceRegistry);
+        } catch (HibernateException ex) {
+            System.err.println("Initial SessionFactory creation failed." + ex);
+            throw new ExceptionInInitializerError(ex);
+        }
+    }
+
+    public static SessionFactory getSessionFactory() {
+        return sessionFactory;
+    }
+
+    public static void closeSessionFactory() {
+        sessionFactory.close();
+    }
+}
